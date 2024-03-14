96865a877f4e5f6c1b30bf9d9b799c2f9da64a54
mkdigitals <mustafa.karadayi@yahoo.com>
2024-03-14 18:05:13 +0300

    number functions completed

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,49 @@
-function add (a, b) {
-  // your code here
+function add(a, b) {
+  return a + b;
 }
 
-function subtract (a, b) {
-  // your code here
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply (a, b) {
-  // your code here
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide (a, b) {
-  // your code here
+function divide(a, b) {
+  return a / b;
 }
 
-function power (a, b) {
-  // your code here
+function power(a, b) {
+  return a ** b;
 }
 
-function round (a) {
-  // your code here
+function round(a) {
+  return Math.round(a);
 }
 
-function roundUp (a) {
-  // your code here
+function roundUp(a) {
+  return Math.ceil(a);
 }
 
-function roundDown (a) {
-  // your code here
+function roundDown(a) {
+  return Math.floor(a);
 }
 
-function absolute (a) {
-  // your code here
+function absolute(a) {
+  if (a >= 0) {
+    return a;
+  }
+  return -1 * a;
 }
 
-function quotient (a, b) {
-  // your code here
+function quotient(a, b) {
+  if (a / b > 0) return Math.floor(a / b);
+  return Math.ceil(a / b);
 }
 
-function remainder (a, b) {
-  // your code here
+function remainder(a, b) {
+  return a - Math.floor(a / b) * b;
 }
 
 module.exports = {
@@ -54,4 +58,4 @@ module.exports = {
   absolute,
   quotient,
   remainder
-}
+};
